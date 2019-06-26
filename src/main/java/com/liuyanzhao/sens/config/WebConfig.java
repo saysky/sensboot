1478de811a965186c70aa00cd239adefdbc9a992
saysky <847064370@qq.com>
2019-06-26 22:40:34 +0800

    支持跨域

---
+++ src/main/java/com/liuyanzhao/sens/config/WebConfig.java
@@ -0,0 +1,32 @@
+package com.liuyanzhao.sens.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+/**
+ * @author 言曌
+ * @date 2019-06-26 22:37
+ */
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private CorsConfiguration buildConfig() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("*");
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.addAllowedHeader("*");
+        corsConfiguration.addAllowedMethod("*");
+
+        return corsConfiguration;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", buildConfig());
+        return new CorsFilter(source);
+    }
+}
