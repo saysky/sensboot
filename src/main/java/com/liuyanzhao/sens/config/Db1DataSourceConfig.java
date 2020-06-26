66f9ee8f0a2604d5466644d24dfd87d64693d4ab
saysky <847064370@qq.com>
2020-06-26 12:06:00 +0800

    update

--- src/main/java/com/liuyanzhao/sens/config/Db1DataSourceConfig.java
+++ src/main/java/com/liuyanzhao/sens/config/Db1DataSourceConfig.java
@@ -10,7 +10,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
--- src/main/java/com/liuyanzhao/sens/config/Db2DataSourceConfig.java
+++ src/main/java/com/liuyanzhao/sens/config/Db2DataSourceConfig.java
@@ -7,7 +7,6 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
@@ -27,7 +26,7 @@ import javax.sql.DataSource;
 public class Db2DataSourceConfig {
 
     @Autowired
-    private Db1DataSourceProperties db2DataSourceProperties;
+    private Db2DataSourceProperties db2DataSourceProperties;
 
     @Bean(name = "db2DataSource")
     public DataSource dataSource() {
